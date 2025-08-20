27f51ac26bc21def54aebf283f4fb814219fbe04
nivea <niveasouza86@gmail.com>
2025-08-19 21:48:14 -0300

    19/08/2025

---
+++ matrizes/atividade2/js/main.js
@@ -0,0 +1,33 @@
+function validaProc() {
+   // Construa um algoritmo para ler uma matriz(2x2)
+   //  de inteiros e imprimir a soma dos elementos
+   //   de uma linha fornecida pelo usuário
+   
+      let matriz = [], resp =[];
+      let linha = 3;
+      let coluna = 3;
+      // Alimentar a matriz
+      for(let i = 0; i < linha;i++){
+         matriz[i]=[]
+         for(let y = 0; y <coluna; y++){
+            matriz[i][y] = parseInt(prompt(`Digite o valor da linha ${i} e da coluna ${y}:`));
+         }
+      }
+      let numero = parseInt(prompt("Informe a linha que deseja imprimir: "))
+
+      for(let i = 0; i < linha;i++){  
+         if(i == numero){
+            for(let y = 0; y <coluna; y++){
+               guardar += matriz[numero][y]  
+               document.write(resp[i][y]+"&nbsp&nbsp");
+            }
+         } 
+         document.write("<br>");
+      }
+      return false;
+     
+}
+
+
+
+
